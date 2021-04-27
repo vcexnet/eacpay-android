7d591e4b7e08fb9b01b15e640f2c456c6894eb7c
vcexnet <36216930+vcexnet@users.noreply.github.com>
2021-04-27 23:37:59 +0800

    update BR_VERSION 635-->650

--- app/src/main/jni/eacpay-core/BRPeer.h
+++ app/src/main/jni/eacpay-core/BRPeer.h
@@ -56,7 +56,7 @@ extern "C" {
 #define SERVICES_NODE_BLOOM   0x04 // BIP111: https://github.com/bitcoin/bips/blob/master/bip-0111.mediawiki
 #define SERVICES_NODE_BCASH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
     
-#define BR_VERSION "635"
+#define BR_VERSION "650"
 #define USER_AGENT "/eacpay-android:" BR_VERSION "/"
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
